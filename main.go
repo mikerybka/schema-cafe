82234601f6af3844d402353721207895226bc3a4
Mike Rybka <merybka@gmail.com>
2025-06-20 20:29:48 -0400

    Read from data dir

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/mikerybka/util"
 )
@@ -22,5 +26,31 @@ type SchemaCafe struct {
 }
 
 func (cafe *SchemaCafe) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, cafe.DataDir)
+	path := filepath.Join(cafe.DataDir, r.URL.Path)
+	fi, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			http.NotFound(w, r)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if fi.IsDir() {
+		entries, err := os.ReadDir(path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		for _, e := range entries {
+			fmt.Fprintf(w, "<a href=\"%s\">%s</a>", filepath.Join(r.URL.Path, e.Name()), e.Name())
+		}
+		return
+	}
+	f, err := os.Open(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.Copy(w, f)
 }
