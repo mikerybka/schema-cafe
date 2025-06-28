ca41d5cb626225c1ff81716db8edb7e70b57105a
Mike Rybka <merybka@gmail.com>
2025-06-28 13:43:12 +0000

    Serve main js and css files in a simple html wrapper

--- main.go
+++ main.go
@@ -3,16 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/mikerybka/util"
+	"os"
 )
 
 func main() {
-	port := util.EnvVar("PORT", "2069")
-	addr := ":" + port
-	err := http.ListenAndServe(addr, nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "2069"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, `<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple Page</title>
+  <link rel="stylesheet" href="/main.css">
+</head>
+<body>
+  <div id="root"></div>
+  <script src="/main.js"></script>
+</body>
+</html>`)
+	})
+
+	http.HandleFunc("/main.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		fmt.Fprint(w, `console.log("Hello from main.js");`)
+	})
+
+	http.HandleFunc("/main.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css")
+		fmt.Fprint(w, `body { font-family: sans-serif; background: #f0f0f0; }`)
+	})
+
+	fmt.Printf("Server listening on port %s...\n", port)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("Error starting server:", err)
 	}
 }
